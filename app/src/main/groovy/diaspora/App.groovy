36bc670051683bc49956423b126181670f79f074
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-27 10:10:18 -0500

    add: get responses from two users, and theirs followers

--- app/src/main/groovy/diaspora/App.groovy
+++ app/src/main/groovy/diaspora/App.groovy
@@ -1,1 +1,10 @@
 package diaspora
+
+import wslite.rest.*
+
+def usernames = ["3rickDJ", "ghector6"]
+
+def client = new RESTClient("https://api.github.com")
+def responses = usernames.collect{client.get(path:"users/$it/followers")}
+println responses*.json*.login
+assert [200,200] == responses*.statusCode
