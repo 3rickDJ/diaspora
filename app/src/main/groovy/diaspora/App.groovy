bb4e8a0e3cb68f2a636845b5e89f61a5d87a4511
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-27 12:11:06 -0500

    Add: add enviroment variables for PAT authorization

--- app/src/main/groovy/diaspora/App.groovy
+++ app/src/main/groovy/diaspora/App.groovy
@@ -3,8 +3,11 @@ package diaspora
 import wslite.rest.*
 
 def usernames = ["3rickDJ", "ghector6"]
-
+def API_KEY = new File('/home/erick/Code/repos/diaspora', 'ENV').collect{it}.join("")
 def client = new RESTClient("https://api.github.com")
-def responses = usernames.collect{client.get(path:"users/$it/followers")}
+def responses = usernames.collect{
+    client.get(path:"users/$it/followers", headers:["Authorization" :"token $API_KEY"])
+    }
 println responses*.json*.login
 assert [200,200] == responses*.statusCode
+println responses[0].json*.login.intersect(responses[1].json*.login)
