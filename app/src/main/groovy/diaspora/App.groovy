2ecc153070e65bc2c38ad08723308ecba4808e12
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-28 19:36:13 -0500

    add: main class, getting graph of followers in GitHub

--- app/src/main/groovy/diaspora/App.groovy
+++ app/src/main/groovy/diaspora/App.groovy
@@ -1,13 +1,17 @@
 package diaspora
-
 import wslite.rest.*
 
-def usernames = ["3rickDJ", "ghector6"]
-def API_KEY = new File('/home/erick/Code/repos/diaspora', 'ENV').collect{it}.join("")
-def client = new RESTClient("https://api.github.com")
-def responses = usernames.collect{
-    client.get(path:"users/$it/followers", headers:["Authorization" :"token $API_KEY"])
-    }
-println responses*.json*.login
-assert [200,200] == responses*.statusCode
-println responses[0].json*.login.intersect(responses[1].json*.login)
+
+List usernames = ["3rickDJ", "ghector6"]
+String API_KEY = new File('/home/erick/Code/repos/diaspora', 'ENV').collect{it}.join("")
+def GitHubfollowers = new UtilFollowers('https://api.github.com')
+GitHubfollowers.token = API_KEY
+List commonfollowers = GitHubfollowers.getCommonFollowers(usernames)
+
+List relationshipCommonFollowers = GitHubfollowers.getFollowRelation(commonfollowers)
+def graficar = new Graph()
+String FollowersOfFollowers = graficar.graphvizFollowersOfFollowers(relationshipCommonFollowers)
+String CommonFollowers = graficar.graphvizFollowers(usernames, commonfollowers)
+
+println FollowersOfFollowers
+println CommonFollowers
