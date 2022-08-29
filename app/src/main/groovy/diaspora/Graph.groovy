f1dfdc6df4a3d763601914ad7a463fd078a18f2d
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-28 19:31:14 -0500

    refactor: formating output methods in Graph class
    graphviz detect as a error any hyphen or number if it is not enclosed
    by quotation marks

--- app/src/main/groovy/diaspora/Graph.groovy
+++ app/src/main/groovy/diaspora/Graph.groovy
@@ -3,14 +3,14 @@ class Graph {
     String graphvizFollowersOfFollowers(List relations){
         relations.collect{relation ->
             relation.collect{user,followers->
-                followers.collect{follower -> "$follower -> $user;"}.join("\n")
+                followers.collect{follower -> "\"$follower\" -> \"$user\";"}.join("\n")
             }.join("\n")
         }.join("\n")
     }
     String graphvizFollowers(List users, List commonFollowers){
         users.collect{user->
             commonFollowers.collect{follower->
-                "$follower -> $user;"
+                "\"$follower\" -> \"$user\";"
             }.join("\n")
         }.join("\n")
     }
